b00dca79e598d0f4eec6bd38e6902c823540f6c0
Usman Sami <usami.bscs25seecs@seecs.edu.pk>
2026-07-01 21:22:59 +0500

    Modified the code

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -6,37 +6,47 @@ print("""=======================================================================
 print("""RULES:
                 1) Computer will generate a random number from 1 to 100
                 2) A player can enter his guess
-                3) Based on that guessed number, hints will be given like too big for >10, too small for <10 or close
+                3) Based on that guessed number, hints will be given like too big or too small for difference of 10 or more
+                   Close will be given in case of difference less than 10 
                 4) There are no limits for guesses
                 5) Total number of guesses will be shown at the end""")
-print("_______________________________________________________________________________________________________\n")
+line = "_"*100
+print(line)
 number = random.randint(1,100)
 print("The number has been generated. Lets play the game!")
 winning = True
 guess = 0
 while(winning):
-    num = int(input("Enter your number : "))
+    try:
+        num = int(input("Enter your number : "))
+    except ValueError:
+        print("Please enter a valid number.")
+    if num<1 or num>100:
+        print("Please enter a number between 1 and 100.")
+        continue
+    difference = abs(num-number)
     if(num == number):
+        guess+=1
         print("Congrats you have guessed correctly!")
         print("The number of guesses used are",guess)
-        print("_______________________________________________________________________________________________________\n")
-
+        line = "_"*100
+        print(line)
         winning = False
-    if (num>number and (num-number)>=10):
+        break
+    elif (num > number and difference >= 10):
         print("The guessed number is too big!")
-        print("_______________________________________________________________________________________________________\n")
+        print(line)
         guess+=1
-    elif (num>number and (num-number)<10):
+
+    elif (difference < 10):
         print("The guessed number is close!\nYou are in the right direction")
-        print("_______________________________________________________________________________________________________\n")
+        print(line)
         guess+=1
-    elif (num<number and (number-num)>=10):
+
+    elif (num < number and difference >= 10):
         print("The guessed number is too small!")
-        print("_______________________________________________________________________________________________________\n")
+        print(line)
         guess+=1
-    elif (num<number and (number-num)<10):
-        print("The guessed number is close!\nYou are in the right direction")
-        print("_______________________________________________________________________________________________________\n")
-        guess+=1
+   
     
     
